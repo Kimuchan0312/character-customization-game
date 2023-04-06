5871cfaac4297ae9b0b3d2d6d0e5be04dbf08b25
Kimuchan0312 <75709261+Kimuchan0312@users.noreply.github.com>
2023-04-06 13:08:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -18,6 +18,21 @@ const total = {
   layer_3: 9,
 };
 
+function multiply(a,b) {
+  return a*b;
+}
+
+function square(x) {
+  return multiply(x,x)
+}
+
+function run(fn) {
+  console.log(`running ${fn}`);
+  console.log(`Result: ${fn(10)}`);
+  console.log('Done');
+}
+
+run(square)
 
 const generateCustomizationOptions = (category, count, setSelectedOptions) => {
   const options = [];
